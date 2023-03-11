5ed138f6ce2ad430debd5d33ed8fe68a70552510
Fkajose <68516668+Fkajose@users.noreply.github.com>
2023-03-11 04:04:43 +0100

    Fixed all bugs in day 52 code!

--- 52. Instagram Follower Bot/main.py
+++ 52. Instagram Follower Bot/main.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import ElementClickInterceptedException
 import os
 from time import sleep
 
@@ -16,92 +15,83 @@ PASSWORD = os.environ.get("TEST_IG_PASSWORD")
 IG_LOGIN_PAGE = "https://www.instagram.com/accounts/login/"
 
 
-def __init__(self, driver_path):
+class InstaFollower:
     """A class to automate following Instagram followers of a given account.
 
-    Attributes:
-        driver_path (str): The file path to the Chrome driver.
+        Attributes:
+            driver_path (str): The file path to the Chrome driver.
 
-    Methods:
-        login(url, username, password): Logs into Instagram with the given username and password.
-        find_followers(account): Navigates to the followers popup of the given account and scrolls through it.
-        follow(): Follows all users listed in the followers popup.
-    """
+        Methods:
+            login(url, username, password): Logs into Instagram with the given username and password.
+            find_followers(account): Navigates to the followers popup of the given account and scrolls through it.
+            follow(): Follows all users listed in the followers popup.
+        """
 
-    self.service = Service(driver_path)
-    self.driver = webdriver.Chrome(service=self.service)
+    def __init__(self, driver_path):
+        self.service = Service(driver_path)
+        self.driver = webdriver.Chrome(service=self.service)
 
+    def login(self, url, username, password):
+        """
+        Method to login to Instagram account
 
-def login(self, url, username, password):
-    """
-    Method to login to Instagram account
+        Args:
+        url (str): Instagram login page URL
+        username (str): Instagram account username
+        password (str): Instagram account password
+        """
+        self.driver.get(url)
 
-    Args:
-    url (str): Instagram login page URL
-    username (str): Instagram account username
-    password (str): Instagram account password
-    """
-    self.driver.get(url)
+        sleep(3)
 
-    sleep(3)
+        # Enter username
+        username_field = self.driver.find_element(By.NAME, "username")
+        username_field.send_keys(username)
 
-    # Enter username
-    username_field = self.driver.find_element(By.NAME, "username")
-    username_field.send_keys(username)
+        sleep(1)
 
-    sleep(1)
+        # Enter password
+        password_field = self.driver.find_element(By.NAME, "password")
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.ENTER)
 
-    # Enter password
-    password_field = self.driver.find_element(By.NAME, "password")
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.ENTER)
+        sleep(5)
 
-    sleep(5)
+    def find_followers(self, account):
+        """
+        Method to find the followers of an Instagram account
 
-
-def find_followers(self, account):
-    """
-    Method to find the followers of an Instagram account
-
-    Args:
-    account (str): Instagram account to find the followers of
-    """
-    url = f"https://www.instagram.com/{account}/followers/"
-    self.driver.get(url)
-    sleep(2)
-
-    # Get the followers popup element
-    followers_popup = self.driver.find_element(
-        By.XPATH, '//*[@id="mount_0_0_qa"]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]/div[2]')
-
-    # Scroll the followers popup 20 times to load more followers
-    for i in range(20):
-        self.driver.execute_script(
-            "arguments[0].scrollTop = arguments[0].scrollHeight", followers_popup)
+        Args:
+        account (str): Instagram account to find the followers of
+        """
+        url = f"https://www.instagram.com/{account}/followers/"
+        self.driver.get(url)
         sleep(2)
 
+        # Get the followers popup element
+        followers_popup = self.driver.find_element(
+            By.CSS_SELECTOR, '._aano')
 
-def follow(self):
-    """
-    Method to follow all the followers of an Instagram account
-    """
-    all_buttons = self.driver.find_elements(By.CSS_SELECTOR, "li button")
-    for button in all_buttons:
-        try:
+        # Scroll the followers popup 10 times to load more followers
+        for i in range(10):
+            self.driver.execute_script(
+                "arguments[0].scrollTop = arguments[0].scrollHeight", followers_popup)
+            sleep(2)
+
+    def follow(self):
+        """
+        Method to follow all the followers of an Instagram account
+        """
+        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, "button")
+        print(f"Accounts found: {len(all_buttons)}")
+
+        num_followed = 0
+        for button in all_buttons:
             if button.text == "Follow":
                 button.click()
                 sleep(1)
-
-        except ElementClickInterceptedException:
-            # If the follow button is not clickable due to an interception,
-            # click the cancel button to dismiss the popup
-            cancel_button = self.driver.find_element_by_xpath(
-                '/html/body/div[5]/div/div/div/div[3]/button[2]')
-            cancel_button.click()
-
-    # Scroll to the bottom of the page to load more followers
-    self.driver.execute_script(
-        "argument[0].scrollIntoView(true);", all_buttons[-1])
+                num_followed += 1
+        print(f"Followed {num_followed} accounts in this run")
 
 
 bot = InstaFollower(CHROME_DRIVER_PATH)
