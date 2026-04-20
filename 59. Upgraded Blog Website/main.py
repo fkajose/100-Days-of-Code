5fe6c5bb05c9b3beec50ab27bd61b3d6a1505304
Fkajose <68516668+Fkajose@users.noreply.github.com>
2026-04-20 22:50:04 +0100

    Complete day 59: Enhance blog functionality and update branding in templates

--- 59. Upgraded Blog Website/main.py
+++ 59. Upgraded Blog Website/main.py
@@ -10,7 +10,14 @@ def home():
     blog_url = "https://api.npoint.io/7ffde18fb59256cc0932"
     response = requests.get(blog_url)
     all_posts = response.json()
-    return render_template("index.html")
+    return render_template("index.html", all_posts=all_posts)
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    post_url = f"https://api.npoint.io/7ffde18fb59256cc0932/{post_id-1}"
+    response = requests.get(post_url)
+    selected_post = response.json()
+    return render_template("post.html", post=selected_post)
 
 @app.route('/about')
 def about():
