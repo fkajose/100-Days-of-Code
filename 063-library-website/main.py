cb581ad0d75e35b1fee6c269b7f6125e6d9bdad1
Fkajose <68516668+Fkajose@users.noreply.github.com>
2026-04-24 01:07:00 +0100

    Initialize day 63: library project

---
+++ 063-library-website/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    pass
+
+
+@app.route("/add")
+def add():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
