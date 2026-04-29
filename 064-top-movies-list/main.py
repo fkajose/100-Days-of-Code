422559be5edbbe1af7278416bfdbd81bbda34a38
Fkajose <68516668+Fkajose@users.noreply.github.com>
2026-04-30 00:00:32 +0100

    Day 64: add movie addition functionality with form and update templates

--- 064-top-movies-list/main.py
+++ 064-top-movies-list/main.py
@@ -1,7 +1,7 @@
 # from unittest.mock import Base
 
 from flask import Flask, render_template, redirect, url_for, request
-from flask_bootstrap import Bootstrap5
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Float, desc
@@ -13,7 +13,7 @@ import requests
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
-Bootstrap5(app)
+Bootstrap(app)
 
 
 # CREATE DB
@@ -48,6 +48,11 @@ class RateMovieForm(FlaskForm):
     submit = SubmitField("Done")
 
 
+class AddMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
+
 @app.route("/")
 def home():
     result = db.session.execute(db.select(Movie).order_by(Movie.ranking.desc()))
@@ -75,6 +80,23 @@ def delete():
     db.session.delete(movie)
     db.session.commit()
     return redirect(url_for("home"))
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    form = AddMovieForm()
+    if form.validate_on_submit():
+        return redirect(url_for("home"))
+    return render_template("add.html", form=form)
+    # if request.method == "POST":
+    #     movie_title = request.form.get("title")
+    #     response = requests.get(
+    #         "https://api.themoviedb.org/3/search/movie",
+    #         params={"api_key": "a9c1e1c8d0f2b4e7aabf0cfeeea9cde", "query": movie_title},
+    #     )
+    #     data = response.json()["results"]
+    #     return render_template("select.html", options=data)
+    # return render_template("add.html", )
 
 
 if __name__ == "__main__":
