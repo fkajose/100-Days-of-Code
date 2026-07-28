0f5978e92b01389964be55239ccd8139e72b7738
Fkajose <68516668+Fkajose@users.noreply.github.com>
2026-07-28 01:11:41 +0100

    Day 69: Initialise Upgraded Blog Project
    - Added about.html for the About Me page with placeholder content.
    - Implemented contact.html with a contact form and success message handling.
    - Developed footer.html for consistent footer across pages.
    - Created header.html for navigation and site structure.
    - Added index.html for displaying all blog posts with links to individual posts.
    - Implemented login.html for user login interface.
    - Created make-post.html for creating and editing blog posts.
    - Developed post.html for displaying individual blog posts and comments.
    - Added register.html for user registration interface.

---
+++ 069-blog-with-users/forms.py
@@ -0,0 +1,27 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+# TODO: Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign Up")
+
+
+# TODO: Create a LoginForm to login existing users
+
+
+# TODO: Create a CommentForm so users can leave comments below posts
